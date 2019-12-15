2ca1a6f7612ac4d2822621d16e45240e186f4695
Patrik Olin <enmejl@gmail.com>
2019-12-15 22:24:33 +0100

    finished scraping function and added write_to_file function

--- lib/bible_scraper.ex
+++ lib/bible_scraper.ex
@@ -8,10 +8,10 @@ defmodule BibleScraper do
           body
           |> Floki.find("a")
           |> Floki.attribute("href")
-          # |> Enum.each(fn x -> "http://runeberg.org/bibeln/" <> x end)
+          |> Enum.map(fn x -> "http://runeberg.org/bibeln/" <> x end)
           # |> Enum.each(fn x -> IO.puts x end)
-          |> Enum.map(fn(url) -> HTTPoison.get!("http://runeberg.org/bibeln/" <> url) end)
-          
+          # |> Enum.map(fn(url) -> HTTPoison.get!("http://runeberg.org/bibeln/" <> url) end)
+          # |> Enum.map(fn {_, result} -> result.body end)
 
 
       {:ok, urls}
@@ -21,5 +21,41 @@ defmodule BibleScraper do
         IO.inspect reason
     end
   end
+
+  def get_chapter_body({_, urls}) do
+    urls
+    |> Enum.map(fn url -> HTTPoison.get(url) end)
+    |> Enum.map(fn {_, result} -> result.body end)
+  end
+
+  def get_bible_text(body) do
+    body
+      |> Floki.find("pre")
+      |> Floki.text()
+  end
+
+
+  def get_bible() do
+    bible =
+      get_chapter_url()
+      |> get_chapter_body()
+      |> Enum.map(fn body ->
+         %{
+           text: get_bible_text(body)
+         }
+         end)
+
+    {:ok, bible}
+  end
+
+  def write_bible_to_file({_, bible}) do
+    file = File.open!("bible", [:read, :utf8, :write])
+    bible
+      |> Enum.each(fn b ->
+          IO.puts(file, b.text)
+        end
+        )
+  end
+
 
 end
